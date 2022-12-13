cf5ea1f4f95be00af08492715fe32504964d2938
Joellensilva <joellen.absilva@gmail.com>
2022-12-13 15:32:57 -0300

    testes

--- src/parser_test.py
+++ src/parser_test.py
@@ -6,74 +6,71 @@ import json
 
 
 class TestParser(unittest.TestCase):
-    # def test_05_2020(self):
-    #     self.maxDiff = None
-    #     # Json com a saida esperada
-    #     with open('src/output_test/expected/expected_05_2020.json', 'r') as fp:
-    #         expected = json.load(fp)
+    def test_05_2020(self):
+        self.maxDiff = None
+        # Json com a saida esperada
+        with open('src/output_test/expected/expected_05_2020.json', 'r') as fp:
+            expected = json.load(fp)
 
-    #     files = ['src/output_test/sheets/membros-ativos-contracheques-05-2020.xls',
-    #              'src/output_test/sheets/membros-ativos-indenizacoes-05-2020.ods']
-    #     dados = data.load(files, '2020', '05', 'src/output_test/sheets')
-    #     result_data = parse(dados, 'mpt/05/2020')
-    #     # Converto o resultado do parser, em dict
-    #     result_to_dict = MessageToDict(result_data)
+        files = ['src/output_test/sheets/membros-ativos-contracheques-05-2020.xls',
+                 'src/output_test/sheets/membros-ativos-indenizacoes-05-2020.ods']
+        dados = data.load(files, '2020', '05', 'src/output_test/sheets')
+        result_data = parse(dados, 'mpt/05/2020')
+        # Converto o resultado do parser, em dict
+        result_to_dict = MessageToDict(result_data)
 
-    #     self.assertEqual(expected['contraCheque'][0],
-    #                      result_to_dict['contraCheque'][0])
+        self.assertEqual(expected['contraCheque'][0],
+                         result_to_dict['contraCheque'][0])
 
-    # def test_06_2020(self):
-    #     self.maxDiff = None
-    #     # Json com a saida esperada
-    #     with open('src/output_test/expected/expected_06_2020.json', 'r') as fp:
-    #         expected = json.load(fp)
+    def test_06_2020(self):
+        self.maxDiff = None
+        # Json com a saida esperada
+        with open('src/output_test/expected/expected_06_2020.json', 'r') as fp:
+            expected = json.load(fp)
 
-    #     files = ['src/output_test/sheets/membros-ativos-contracheques-06-2020.xls',
-    #              'src/output_test/sheets/membros-ativos-indenizacoes-06-2020.ods']
-    #     dados = data.load(files, '2020', '06', 'src/output_test/sheets')
-    #     result_data = parse(dados, 'mpt/06/2020')
-    #     # Converto o resultado do parser, em dict
-    #     result_to_dict = MessageToDict(result_data)
+        files = ['src/output_test/sheets/membros-ativos-contracheques-06-2020.xls',
+                 'src/output_test/sheets/membros-ativos-indenizacoes-06-2020.ods']
+        dados = data.load(files, '2020', '06', 'src/output_test/sheets')
+        result_data = parse(dados, 'mpt/06/2020')
+        # Converto o resultado do parser, em dict
+        result_to_dict = MessageToDict(result_data)
 
-    #     self.assertEqual(expected['contraCheque'][0],
-    #                      result_to_dict['contraCheque'][0])
+        self.assertEqual(expected['contraCheque'][0],
+                         result_to_dict['contraCheque'][0])
 
-    # def test_07_2020(self):
-    #     self.maxDiff = None
-    #     # Json com a saida esperada
-    #     with open('src/output_test/expected/expected_07_2020.json', 'r') as fp:
-    #         expected = json.load(fp)
+    def test_07_2020(self):
+        self.maxDiff = None
+        # Json com a saida esperada
+        with open('src/output_test/expected/expected_07_2020.json', 'r') as fp:
+            expected = json.load(fp)
 
-    #     files = ['src/output_test/sheets/membros-ativos-contracheques-07-2020.xls',
-    #              'src/output_test/sheets/membros-ativos-indenizacoes-07-2020.ods']
-    #     dados = data.load(files, '2020', '07', 'src/output_test/sheets')
-    #     result_data = parse(dados, 'mpt/07/2020')
-    #     # Converto o resultado do parser, em dict
-    #     result_to_dict = MessageToDict(result_data)
+        files = ['src/output_test/sheets/membros-ativos-contracheques-07-2020.xls',
+                 'src/output_test/sheets/membros-ativos-indenizacoes-07-2020.ods']
+        dados = data.load(files, '2020', '07', 'src/output_test/sheets')
+        result_data = parse(dados, 'mpt/07/2020')
+        # Converto o resultado do parser, em dict
+        result_to_dict = MessageToDict(result_data)
 
-    #     self.assertEqual(expected['contraCheque'][0],
-    #                      result_to_dict['contraCheque'][0])
+        self.assertEqual(expected['contraCheque'][0],
+                         result_to_dict['contraCheque'][0])
 
-    # def test_04_2020(self):
-    #     self.maxDiff = None
-    #     # Json com a saida esperada
-    #     with open('src/output_test/expected/expected_04_2020.json', 'r') as fp:
-    #         expected = json.load(fp)
+    def test_04_2020(self):
+        self.maxDiff = None
+        # Json com a saida esperada
+        with open('src/output_test/expected/expected_04_2020.json', 'r') as fp:
+            expected = json.load(fp)
 
-    #     files = ['src/output_test/sheets/membros-ativos-contracheques-04-2020.xls',
-    #              'src/output_test/sheets/membros-ativos-indenizacoes-04-2020.ods']
-    #     dados = data.load(files, '2020', '04', 'src/output_test/sheets')
-    #     result_data = parse(dados, 'mpt/04/2020')
+        files = ['src/output_test/sheets/membros-ativos-contracheques-04-2020.xls',
+                 'src/output_test/sheets/membros-ativos-indenizacoes-04-2020.ods']
+        dados = data.load(files, '2020', '04', 'src/output_test/sheets')
+        result_data = parse(dados, 'mpt/04/2020')
 
-    #     # Converto o resultado do parser, em dict
-    #     result_to_dict = MessageToDict(result_data)
+        # Converto o resultado do parser, em dict
+        result_to_dict = MessageToDict(result_data)
 
-    #     self.assertEqual(expected['contraCheque'][0],
-    #                      result_to_dict['contraCheque'][0])
-    
-    
-    
-    ################################## FALTA FINALIZAAAR
+        self.assertEqual(expected['contraCheque'][0],
+                         result_to_dict['contraCheque'][0])
+
     def test_05_2019(self):
         self.maxDiff = None
         # Json com a saida esperada
